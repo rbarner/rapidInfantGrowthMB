a3cb6e7101f2c49c1a7ff1ad180b8e7577b9aba5
Roshonda B. Jones <rjones@Roshondas-MacBook-Pro.local>
2020-08-28 00:39:08 -0700

    Adding necessary package and updating getter functions

--- R/getterAndPrint_functions.R
+++ R/getterAndPrint_functions.R
@@ -55,3 +55,31 @@ print_lefse_LDA_graph <- function(filepath)
   plot_lefse_LDA_graph()
   graphics.off()
 }
+
+print_intercept_slope_taxa_growthMeasures_association <- function(taxaOfInterest = "k__Bacteria| p__Proteobacteria| c__Gammaproteobacteria| o__Pseudomonadales", measureOfInterest = "inf_weight_kg_12m",filepath)
+{
+  load("data/metaData_generalCharacteristics.RData") #loads in "dataSet" which is a data frame containing metadata
+  load("data/lefse_allData_rapidGrowth_updated_notLogged.RData") #loads in "myT" which is a data frame with lefse_formatted taxa counts per sample
+
+  taxaMeta <- merge(metaData,lefseTaxaCounts,by.x="dyad_id",by.y="row.names")
+
+  thisDataInstance <- taxaMeta[,names(taxaMeta) %in% c(taxaOfInterest,"k__Bacteria","baby_birthlength_cm","baby_birthweight_kg","gestational_age_category",measureOfInterest,"dyad_id")]
+  thisDataInstance$dyad_id <- factor(thisDataInstance$dyad_id)
+  thisDataInstance$gestational_age_category <- factor(thisDataInstance$gestational_age_category,levels = c("On Time","Late","Early"))
+
+  names(thisDataInstance)[ names(thisDataInstance) %in% measureOfInterest] <- "thisVariable"
+  names(thisDataInstance)[ names(thisDataInstance) %in% taxaOfInterest] <- "thisTaxa"
+
+  thisDataInstance$taxaRelAbundance <- as.numeric(as.character(thisDataInstance$thisTaxa))/as.numeric(as.character(thisDataInstance$k__Bacteria))
+  thisDataInstance$logTaxaRelAbundance <- log10(((as.numeric(as.character(thisDataInstance$thisTaxa))/as.numeric(as.character(thisDataInstance$k__Bacteria)))*mean(as.numeric(as.character(thisDataInstance$k__Bacteria))))+1)
+
+  modelForm=as.formula(paste("thisVariable","~logTaxaRelAbundance+baby_birthlength_cm+baby_birthweight_kg+gestational_age_category"  ));
+  modelInfo <- lm(modelForm, data = thisDataInstance)
+  coefs <- coef(modelInfo)
+
+  lineData <- data.frame(slope = coefs["logTaxaRelAbundance"],intercept = coefs["(Intercept)"]+mean(thisDataInstance$baby_birthlength_cm,na.rm=TRUE)*coefs["baby_birthlength_cm"] +  mean(thisDataInstance$baby_birthweight_kg)*coefs["baby_birthweight_kg"])
+  write.table(lineData,filepath,quote=FALSE, sep="\t",append=FALSE, row.names=FALSE, col.names=TRUE)
+
+}
+
+
